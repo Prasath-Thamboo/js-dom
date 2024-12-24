77cf7a14f4f83a6a03de0778ecfff7ab3ec942ad
Prasath.Thamboo <prasath1@hotmail.fr>
2024-12-24 19:23:23 +0100

    first-commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import { Questions } from './question'
+
+const app = document.querySelector("#app")
+const startButton = document.querySelector("#start")
+
+function startQuizz (){
+  let score = 0
+  let currentQuestion
+}
