2dfe71412f07bb065b07055bba8a951dc8beaf77
Prasath.Thamboo <prasath1@hotmail.fr>
2024-12-26 07:32:46 +0100

    first-commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,17 @@ const startButton = document.querySelector("#start")
 
 function startQuizz (){
   let score = 0
-  let currentQuestion
+  let currentQuestion = 0 
+
+  displayQiestion (currentQuestion)
+
+  function displayQiestion (index){
+    const question = Questions[index]
+
+    if (!question){
+
+    }
+
+    const title = getTitle
+  }
 }
